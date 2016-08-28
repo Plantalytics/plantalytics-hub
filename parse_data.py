568b68f5a290cfe425d91bb7ceec0d3511d8484e
Steven Ngo <stevenngo36@yahoo.com>
2016-08-28 13:04:45 -0700

    Update parse_data.py
    Added header.

--- parse_data.py
+++ parse_data.py
@@ -1,3 +1,10 @@
+# Plantalytics
+#     Copyright (c) 2016 Sapphire Becker, Katy Brimm, Scott Ewing, 
+#       Matt Fraser, Kelly Ledford, Michael Limb, Steven Ngo, Eric Turley.
+#     This project is licensed under the MIT License.
+#     Please see the file LICENSE in this distribution for license terms.
+# Contact: plantalytics.capstone@gmail.com
+
 import ConfigParser
 import json
 import requests
